8c2868bfe7d1568f2c5415137ea999df8c5cc883
ColinChan520 <104440913+ColinChan520@users.noreply.github.com>
2024-04-04 15:15:33 +1030

    Adjust the spacing between each div on the login page and add an "Agree to Terms of Business" confirmation button.

--- Employee MS/src/Components/Login.jsx
+++ Employee MS/src/Components/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './style.css'
 
 const Login = () => {
   return (
@@ -6,15 +7,19 @@ const Login = () => {
         <div className = "p-3 rounded w-25 border loginForm">
             <h2>Login Page</h2>
             <form>
-                <div>
+                <div className='mb-3'>
                     <label htmlFor = "email"><strong>Email:</strong></label>
                     <input type = "email" name = "email" autoComplete='off' placeholder = "Enter your email" className = "form-control rounded-0"/>
                 </div>
-                <div>
+                <div className='mb-3'>
                     <label htmlFor = "password"><strong>Password:</strong></label>
                     <input type = "password" name = "password" placeholder = "Enter your password" className = "form-control rounded-0"/>
                 </div>
-                <button className = "btn btn-success w-100 rounded-0">Submit</button>
+                <button className = "btn btn-success w-100 rounded-0 mb-2">Log in</button>
+                <div className='mb-1'>
+                    <input type = "checkbox" name="tick" id="tick" className='me-2'/>
+                    <label htmlFor = "password">You are agree with terms & conditions</label>
+                </div>
             </form>
         </div>
     </div>
