7ce2aef2fd556a0eb20c6bfd29bab47956b62386
ColinChan520 <104440913+ColinChan520@users.noreply.github.com>
2024-04-08 11:27:07 +0930

    Create a useState temporary storage hook to temporarily store the email and password values for the log in page

--- Employee MS/src/Components/Login.jsx
+++ Employee MS/src/Components/Login.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import './style.css'
 
 const Login = () => {
+
+    const [email, setValue] = useState({
+        email: '',
+        password: ''
+    })
+
   return (
     <div className = "d-flex justify-content-center align-items-center vh-100 loginPage">
         <div className = "p-3 rounded w-25 border loginForm">
@@ -9,11 +15,13 @@ const Login = () => {
             <form>
                 <div className='mb-3'>
                     <label htmlFor = "email"><strong>Email:</strong></label>
-                    <input type = "email" name = "email" autoComplete='off' placeholder = "Enter your email" className = "form-control rounded-0"/>
+                    <input type = "email" name = "email" autoComplete='off' placeholder = "Enter your email" 
+                    onChange={(e) => setValue({...values, email : e.target.value})} className = "form-control rounded-0"/>
                 </div>
                 <div className='mb-3'>
                     <label htmlFor = "password"><strong>Password:</strong></label>
-                    <input type = "password" name = "password" placeholder = "Enter your password" className = "form-control rounded-0"/>
+                    <input type = "password" name = "password" placeholder = "Enter your password" 
+                    onChange={(e) => setValue({...values, password : e.target.value})} className = "form-control rounded-0"/>
                 </div>
                 <button className = "btn btn-success w-100 rounded-0 mb-2">Log in</button>
                 <div className='mb-1'>
